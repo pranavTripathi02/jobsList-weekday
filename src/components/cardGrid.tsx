2c3a610091d8f802b674c22cf9d89ecfd21841a3
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-04 04:37:34 +0530

    infinite scroll

--- src/components/cardGrid.tsx
+++ src/components/cardGrid.tsx
@@ -1,28 +1,55 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Tjob, useGetJobsMutation } from "../store/api/apiSlice";
 import { Container, Stack } from "@mui/material";
 import JobCard from "./card/jobCard";
-import LoadingSkel from "./loadingSkel";
+import LoadingCards from "./loadingCards";
 
 function CardGrid() {
   const [offset, setOffset] = useState(0);
-  const [getJobs, { isLoading }] = useGetJobsMutation();
+  const [getJobs, { isLoading, status }] = useGetJobsMutation();
   const [jobs, setJobs] = useState<Tjob[]>([]);
 
+  const observerTarget = useRef<HTMLDivElement>(null);
+
+  // fetch jobs from api using offset
   const fetchJobs = async () => {
-    try {
-      const res = await getJobs({ offset }).unwrap();
-      console.log(res);
-      setJobs((prevJobs) => prevJobs.concat(res.jobs));
-      setOffset((prev) => prev + 10);
-    } catch (err) {
-      console.error("Error fetching jobs", err);
+    if (!isLoading) {
+      try {
+        const res = await getJobs({ offset }).unwrap();
+        setJobs((prevJobs) => [...prevJobs, ...res.jobs]);
+        console.log("fetching heres 2", status);
+      } catch (err) {
+        console.error("Error fetching jobs", err);
+      }
     }
   };
 
+  // update offset when scrolled to end of page
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setOffset((prev) => prev + 10);
+        }
+      },
+      { threshold: 1 },
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
+
+  // fetchJobs on offset change
   useEffect(() => {
     fetchJobs();
-  }, []);
+  }, [offset]);
 
   return (
     <Container maxWidth="xl">
@@ -39,7 +66,8 @@ function CardGrid() {
               jobInfo={job}
             />
           ))}
-          {isLoading && Array(4).map((_, idx) => <LoadingSkel key={idx} />)}
+          {isLoading && <LoadingCards />}
+          <div ref={observerTarget}></div>
         </Stack>
       )}
     </Container>
