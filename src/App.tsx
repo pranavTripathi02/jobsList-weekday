8f827b5e0a3283856cc93d097543e8499803d953
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-03 06:37:21 +0530

    rtk query init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useGetJobsMutation } from "./store/api/apiSlice";
 
 function App() {
-  return <>Hi</>;
+  const [getJobs, { isLoading }] = useGetJobsMutation();
+  const [offset, setOffset] = useState(0);
+
+  const fetchJobs = async () => {
+    try {
+      const res = await getJobs({ offset });
+      console.log(res);
+      setOffset((prev) => prev + 10);
+    } catch (err) {
+      console.error("Error fetching jobs", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  return <>{isLoading ? <div>Loading...</div> : <div>Got Jobs!</div>}</>;
 }
 
 export default App;
---
+++ src/store/api/apiSlice.ts
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export type Tjob = {
+  jdUid: string;
+  jdLink: string;
+  jobDetailsFromCompany: string;
+  maxJdSalary: number;
+  minJdSalary: number;
+  salaryCurrencyCode: string;
+  location: string;
+  minExp: number | null;
+  maxExp: number | null;
+  jobRole: string;
+};
+
+export const apiSlice = createApi({
+  reducerPath: "jobs",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.weekday.technology/adhoc",
+  }),
+  endpoints: (builder) => ({
+    getJobs: builder.mutation<
+      { jobs: Tjob[]; totalCount: number },
+      { offset: number }
+    >({
+      query: ({ offset }: { offset: number }) => ({
+        url: "/getSampleJdJSON",
+        method: "POST",
+        credentials: "same-origin",
+        headers: { "Content-Type": "application/json" },
+        body: { limit: 10, offset },
+      }),
+      transformResponse: (response: { jdList: Tjob[]; totalCount: number }) => {
+        return { jobs: response.jdList, totalCount: response.totalCount };
+      },
+    }),
+  }),
+});
+
+export const { useGetJobsMutation } = apiSlice;
